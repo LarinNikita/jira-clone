f71f794b059842276dfada942f4f72503a13dd36
Larin Nikita <l_nikita2000@mail.ru>
2024-12-31 16:21:24 +0700

    workspaces switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 import { Navigation } from './navigation';
 import { DottedSeparator } from './dotted-separator';
+import { WorkspacesSwitcher } from './workspaces-switcher';
 
 export const Sidebar = () => {
     return (
@@ -10,6 +11,8 @@ export const Sidebar = () => {
             <Link href="/">
                 <Image src="logo.svg" alt="Logo" width={164} height={48} />
             </Link>
+            <DottedSeparator className="my-4" />
+            <WorkspacesSwitcher />
             <DottedSeparator className="my-4" />
             <Navigation />
         </aside>
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -8,49 +8,60 @@ import { sessionMiddleware } from '@/lib/session-middleware';
 
 import { createWorkspaceSchema } from '../schemas';
 
-const app = new Hono().post(
-    '/',
-    zValidator('form', createWorkspaceSchema),
-    sessionMiddleware,
-    async ctx => {
+const app = new Hono()
+    .get('/', sessionMiddleware, async ctx => {
         const databases = ctx.get('databases');
-        const storage = ctx.get('storage');
-        const user = ctx.get('user');
 
-        const { name, image } = ctx.req.valid('form');
-
-        let uploadedImageUrl: string | undefined;
-
-        if (image instanceof File) {
-            const file = await storage.createFile(
-                IMAGES_BUCKET_ID,
-                ID.unique(),
-                image,
-            );
-
-            const arrayBuffer = await storage.getFilePreview(
-                IMAGES_BUCKET_ID,
-                file.$id,
-            );
-
-            uploadedImageUrl = `data:image/pgn;base64,${Buffer.from(
-                arrayBuffer,
-            ).toString('base64')}`;
-        }
-
-        const workspace = await databases.createDocument(
+        const workspace = await databases.listDocuments(
             DATABASE_ID,
             WORKSPACES_ID,
-            ID.unique(),
-            {
-                name,
-                userId: user.$id,
-                imageUrl: uploadedImageUrl,
-            },
         );
 
-        return ctx.json(workspace);
-    },
-);
+        return ctx.json({ data: workspace });
+    })
+    .post(
+        '/',
+        zValidator('form', createWorkspaceSchema),
+        sessionMiddleware,
+        async ctx => {
+            const databases = ctx.get('databases');
+            const storage = ctx.get('storage');
+            const user = ctx.get('user');
+
+            const { name, image } = ctx.req.valid('form');
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id,
+                );
+
+                uploadedImageUrl = `data:image/pgn;base64,${Buffer.from(
+                    arrayBuffer,
+                ).toString('base64')}`;
+            }
+
+            const workspace = await databases.createDocument(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                ID.unique(),
+                {
+                    name,
+                    userId: user.$id,
+                    imageUrl: uploadedImageUrl,
+                },
+            );
+
+            return ctx.json({ data: workspace });
+        },
+    );
 
 export default app;
