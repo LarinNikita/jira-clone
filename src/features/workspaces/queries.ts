dade721f9a4abbe72a577b985e24500552c6c440
Larin Nikita <l_nikita2000@mail.ru>
2025-01-03 15:05:07 +0700

    refactoring server queries

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,33 +1,18 @@
 'use server';
 
-import { cookies } from 'next/headers';
-import { Account, Client, Databases, Query } from 'node-appwrite';
+import { Query } from 'node-appwrite';
 
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from '@/config';
 
 import { getMember } from '@/features/members/utils';
-import { AUTH_COOKIE } from '@/features/auth/constants';
+
+import { createSessionClient } from '@/lib/appwrite';
 
 import { Workspace } from './types';
 
 export const getWorkspaces = async () => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-        const session = await cookies().get(AUTH_COOKIE);
-
-        if (!session) {
-            return {
-                documents: [],
-                total: 0,
-            };
-        }
-
-        client.setSession(session.value);
-        const databases = new Databases(client);
-        const account = new Account(client);
+        const { account, databases } = await createSessionClient();
         const user = await account.get();
 
         const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
@@ -69,17 +54,7 @@ interface getWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: getWorkspaceProps) => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-        const session = await cookies().get(AUTH_COOKIE);
-
-        if (!session) return null;
-
-        client.setSession(session.value);
-        const databases = new Databases(client);
-        const account = new Account(client);
+        const { account, databases } = await createSessionClient();
         const user = await account.get();
 
         const member = await getMember({
